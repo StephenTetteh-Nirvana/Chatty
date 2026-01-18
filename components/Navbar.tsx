d937f7bb13e032e9e8b1f9f3f16f9244dce09a43
StephenTetteh-Nirvana <stephentetteh2616@gmail.com>
2026-01-18 10:00:24 -0800

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => { 
+    return (
+      <nav className="w-full h-12 bg-gray-800 flex items-center px-4">
+        <h1 className="text-white text-lg font-semibold">Welcome To Chatty</h1>
+      </nav>
+    );
+ }
+
+export default Navbar;
